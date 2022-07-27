f633712583c2db86e877fe3d5fc5038246cc08c4
홍현아 <hyeonahong@gmail.com>
2022-07-27 16:39:33 +0900

    react-hook-form : validation, formState를 활용한 에러 확인

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,44 +1,34 @@
 //react-hook-form : form을 쓸 때 발생하는 수많은 useState들을 줄이는 데 도움을 주는 라이브러리
 import { useForm } from 'react-hook-form';
 
-// function ToDoList() {
-//     const [toDo, setToDo] = useState('');
-//     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//         const {
-//             currentTarget: { value },
-//         } = event;
-//         setToDo(value);
-//     };
-//     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//         event.preventDefault();
-//         console.log(toDo);
-//     };
-
-//     return (
-//         <div>
-//             <form onSubmit={onSubmit}>
-//                 <input onChange={onChange} value={toDo} placeholder='Write a to do' />
-//                 <button>Add</button>
-//             </form>
-//         </div>
-//     );
-// }
-
 function ToDoList() {
-    //register를 사용하면 필요한 object가 내장되어 있어 onChange 이벤트 핸들러가 필요없어짐 => 각 input마다 useState를 줄 일이 사라짐
-    //watch form 의 입력값들의 변화를 관찰 할 수 있게 해주는 함수
-    const { register, watch } = useForm();
-    console.log(watch());
+    //handleSubmit : submit과 같은 역할
+    const { register, handleSubmit, formState } = useForm();
+    const onVaild = (data: any) => {
+        console.log(data);
+    };
+    //form 상태와 에러를 확인할 수 있음
+    console.log(formState.errors);
 
     return (
         <div>
-            <form>
-                <input {...register('email')} placeholder='Email' />
-                <input {...register('first_name')} placeholder='First Name' />
-                <input {...register('last_name')} placeholder='Last Name' />
-                <input {...register('username')} placeholder='Username' />
-                <input {...register('password')} placeholder='Password' />
-                <input {...register('password1')} placeholder='Password1' />
+            <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit(onVaild)}>
+                {/* 복잡하게 하나하나 만들어야 했던 것과 달리 간단하게 validation 가능 */}
+                <input {...register('email', { required: true })} placeholder='Email' />
+                <input {...register('first_name', { required: true, minLength: 10 })} placeholder='First Name' />
+                <input {...register('last_name', { required: true })} placeholder='Last Name' />
+                <input {...register('username', { required: true })} placeholder='Username' />
+                <input {...register('password', { required: true })} placeholder='Password' />
+                <input
+                    {...register('password1', {
+                        required: 'Password is required',
+                        minLength: {
+                            value: 5,
+                            message: 'Your password is too short.',
+                        },
+                    })}
+                    placeholder='Password1'
+                />
                 <button>Add</button>
             </form>
         </div>
