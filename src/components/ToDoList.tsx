e57c2827ef78626cddbc1551230c39de2becb3e2
홍현아 <hyeonahong@gmail.com>
2022-08-03 17:59:56 +0900

    recoil의 selecter를 이용한 리스트 분리

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,24 +1,40 @@
 import { useRecoilValue } from 'recoil';
-import { toDoState } from './atoms';
+import { toDoSelector } from './atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
 function ToDoList() {
-    //컴포넌트 분리로 기능이 분리되었으므로 가져와서 쓰는 useRecoil"Value"를 써야함
-    const toDos = useRecoilValue(toDoState);
-    console.log('ToDoList file toDos', toDos);
+    //이제는 원하는 것을 골라서 가져온 것만 렌더링
+    //toDoSelector에서 return하는 게 배열이므로 여기도 배열로 맞춰줘야 함
+    const [toDo, doing, done] = useRecoilValue(toDoSelector);
 
     return (
         <div>
             <h1>To Dos</h1>
             <hr />
             <CreateToDo />
+            <h2>To Do</h2>
             <ul>
-                {toDos.map((toDo) => (
+                {toDo.map((toDo) => (
                     // === <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
                     <ToDo key={toDo.id} {...toDo} />
                 ))}
             </ul>
+            <hr />
+            <h2>Doing</h2>
+            <ul>
+                {doing.map((toDo) => (
+                    <ToDo key={toDo.id} {...toDo} />
+                ))}
+            </ul>
+            <hr />
+            <h2>Done</h2>
+            <ul>
+                {done.map((toDo) => (
+                    <ToDo key={toDo.id} {...toDo} />
+                ))}
+            </ul>
+            <hr />
         </div>
     );
 }
--- src/components/atoms.tsx
+++ src/components/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export interface IToDo {
     text: string;
@@ -9,4 +9,14 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>({
     key: 'toDo',
     default: [],
+});
+
+//selector : atom의 output을 변형시킴. state를 가져다가 뭔가를 리턴.
+export const toDoSelector = selector({
+    key: 'toDoSelector',
+    //get function이 있어야 atom을 받아(get)올 수 있음
+    get: ({ get }) => {
+        const toDos = get(toDoState);
+        return [toDos.filter((toDo) => toDo.category === 'TO_DO'), toDos.filter((toDo) => toDo.category === 'DOING'), toDos.filter((toDo) => toDo.category === 'DONE')];
+    },
 });
